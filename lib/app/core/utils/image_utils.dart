e6c8362c4575fc3899a904114c8a7914a9b4b6c7
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2024-09-27 17:13:48 +0700

    update

--- lib/app/core/utils/image_utils.dart
+++ lib/app/core/utils/image_utils.dart
@@ -1,1 +1,6 @@
-class ImageUtils {}
+class ImageUtils {
+  static const String _assets = "assets/";
+  static const String _icons = '${_assets}icons/';
+  static const String _background = '${_assets}background/';
+  static const String _illustration = '${_assets}illustration/';
+}
