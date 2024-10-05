022c1d39cef04636e96678de3123e44e5a736f9e
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2024-10-05 18:39:09 +0700

    update

--- lib/app/features/market_screen/controller/market_detail_screen_controller.dart
+++ lib/app/features/market_screen/controller/market_detail_screen_controller.dart
@@ -1,6 +1,17 @@
 import 'package:get/get.dart';
+import 'package:nasa_project/app/repository/market_repository/response/market_detail_response.dart';
 
 class MarketDetailScreenController extends GetxController {
+  RxBool isLoading = false.obs;
+
+  Rx<MarketDetailResponse> marketResponseData = MarketDetailResponse().obs;
+
+  @override
+  void onInit() {
+    // getListPlants();
+    super.onInit();
+  }
+
   onBackTap() {
     Get.back();
   }
